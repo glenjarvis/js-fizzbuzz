d43a07ce21083accae55bcffffb18349a8788e2d
Glen Jarvis <glen@glenjarvis.com>
2025-05-16 11:15:16 -0700

    Fix spelling mistake: 'divisible_by' (was 'divisable_by').
    Fix: #9

--- glenjarvis-fizzbuzz/index.js
+++ glenjarvis-fizzbuzz/index.js
@@ -8,7 +8,7 @@ function FizzBuzz(max_count) {
     this.MAX_COUNT = 1000;
   }
 
-  this.divisable_by = function divisable_by(dividend, divisor) {
+  this.divisible_by = function divisible_by(dividend, divisor) {
     if  (divisor) {
       return !(dividend % divisor)
     } else {
@@ -16,16 +16,16 @@ function FizzBuzz(max_count) {
     }
   }
 
-  this.divisable_by_both = function divisable_by_both(dividend) {
-    return this.divisable_by(dividend, 3) && this.divisable_by(dividend, 5)
+  this.divisible_by_both = function divisible_by_both(dividend) {
+    return this.divisible_by(dividend, 3) && this.divisible_by(dividend, 5)
   };
 
   this.say = function say(number) {
-    if (this.divisable_by_both(number)) {
+    if (this.divisible_by_both(number)) {
       return "fizzbuzz";
-    } else if (this.divisable_by(number, 3)) {
+    } else if (this.divisible_by(number, 3)) {
       return "fizz";
-    } else if (this.divisable_by(number, 5)) {
+    } else if (this.divisible_by(number, 5)) {
       return "buzz";
     } else {
       return number;
